923c5db2f11767def436223827ad7427baf1741c
xingcdev <60323726+xingcdev@users.noreply.github.com>
2022-05-23 13:59:52 +0200

    ajouter housing cards en grid

--- client/src/views/index/Housings/Housings.tsx
+++ client/src/views/index/Housings/Housings.tsx
@@ -1,3 +1,4 @@
+import styles from './Housings.module.scss';
 import HousingCard from '../HousingCard/HousingCard';
 
 interface HousingsProps {
@@ -6,8 +7,30 @@ interface HousingsProps {
 
 function Housings(props: HousingsProps) {
 	return (
-		<section>
+		<section className={styles.housingsList}>
 			{/* {props.housings.map((housing: any) => ( */}
+			<HousingCard
+				housingId={1}
+				photo="https://images.unsplash.com/photo-1554995207-c18c203602cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+				price={23000}
+				address="30-32 avenue de la République"
+				postalCode=" 94 800"
+				city="Villejuif"
+				surface={12}
+				rooms={3}
+				cars={2}
+			/>
+			<HousingCard
+				housingId={1}
+				photo="https://images.unsplash.com/photo-1554995207-c18c203602cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+				price={23000}
+				address="30-32 avenue de la République"
+				postalCode=" 94 800"
+				city="Villejuif"
+				surface={12}
+				rooms={3}
+				cars={2}
+			/>
 			<HousingCard
 				housingId={1}
 				photo="https://images.unsplash.com/photo-1554995207-c18c203602cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
