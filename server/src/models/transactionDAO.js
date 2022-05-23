2e9158a59f67c40f38c772dc06d0edfe43a713a3
Mélanie Dang <melanie.dang8@gmail.com>
2022-05-23 15:38:23 +0200

    add requetes on garage, transaction, user, visite

--- server/src/models/transactionDAO.js
+++ server/src/models/transactionDAO.js
@@ -2,7 +2,6 @@ const database = require('../helpers/dbconnect.js');
 
 async function createTransaction(prixVente, pourcentageCommission, idLogement, idClient) {
 
-    
     return new Promise((resolve, reject) => {
         let sql =
             "INSERT INTO Transaction (PrixVente, PourcentageCommission, IdLogement, IDClient) values (?,?,?,?);";
@@ -27,6 +26,37 @@ async function createTransaction(prixVente, pourcentageCommission, idLogement, i
     }).catch((error) => {
         console.log(error);
     });
+}
+
+
+async function updateTransaction(prixVente) {
+    
+    let sql = "UPDATE Transaction SET PrixVente=?;";
+    database.getConnection((error, connection) =>{
+        if(error) console.error("Database connection error on updateTransaction", error.message);
+    connection.query(sql, [prixVente], (error) => {
+        connection.release();
+        if (error) {
+            console.error(error.message);
+            return;
+        }
+    });
+});
+}
+
+async function deleteTransaction(id) {
+    
+    let sql = "DELETE FROM Transaction WHERE IdTransaction=?;";
+    database.getConnection((error, connection) =>{
+        if(error) console.error("Database connection error on deleteTransaction", error.message);
+    connection.query(sql, [id], (error) => {
+        connection.release();
+        if (error) {
+            console.error(error.message);
+            return;
+        }
+    });
+});
 }
 
 module.exports = {
