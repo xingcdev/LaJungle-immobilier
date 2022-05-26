dfea6795f202b1b01e62350907719d9f2c2a6a85
Antoine Després <58844676+antoinedespres@users.noreply.github.com>
2022-05-26 23:14:07 +0200

    Ajout de getAllVisitesForALogement

--- server/src/controllers/visite.js
+++ server/src/controllers/visite.js
@@ -1,59 +1,88 @@
-const db = require("../models/visiteDAO.js");
+const db = require('../models/visiteDAO.js');
 
 async function getVisite(request, response) {
-    let ID = request.query.id || request.visite?.id;
+  let ID = request.query.id || request.visite?.id;
 
-    if (ID === null) {
-        response.status(404).json({ msg: "ID cannot be null" });
-        return;
+  if (ID === null) {
+    response.status(404).json({ msg: 'ID ne peut être nul' });
+    return;
+  }
+
+  let visite = await db.getVisite(ID);
+
+  if (visite === null) {
+    response.status(404).json({ msg: 'Visite non trouvée' });
+    return;
+  }
+
+  if (visite === undefined) {
+    response.status(404).json({ msg: 'Visite non trouvée' });
+  }
+
+  visite = visite[0];
+
+  let Visite = {
+    IdVisite: ID,
+    DateVisite: visite.DateVisite,
+    IdLogement: visite.IdLogement,
+    IdClient: visite.IdClient,
+  };
+
+  response.json(Visite);
+}
+
+async function getAllVisitesForALogement(request, response) {
+  let ID = request.query.id || request.visite?.id;
+
+  if (ID === null) {
+    response.status(404).json({ msg: 'ID ne peut être nul' });
+    return;
+  }
+
+  try {
+    let visites = await db.getAllVisitesForALogement(ID);
+    if (visites) {
+      response.send(
+        visites.map((element) => ({
+          dateHeureVisite: element.dateHeureVisite,
+          idLogement: element.idLogement,
+          idClient: element.idClient,
+        }))
+      );
+    } else {
+      response.status(200).send({ msg: 'Visites non trouvées' });
     }
+  } catch (error) {
+    res.send({ error });
+  }
+}
 
-    let visite = await db.getVisite(ID);
+async function createVisite(request, response) {
+  let visite = request.body;
+  db.createVisite(visite.dateHeureVisite, visite.idLogement, visite.idClient);
 
-    if (visite === null) {
-        response.status(404).json({ msg: "Visite non trouvé" });
-        return;
-    }
-
-    if (visite === undefined) {
-        response.status(404).json({ msg: "Visite non trouvé" });
-    }
-
-    visite = visite[0]
-
-    let Visite = {
-        IdVisite : ID,
-        DateVisite : visite.DateVisite,
-        IdLogement : visite.IdLogement,
-        IdClient : visite.IdClient
-    };
-
-    response.json(Visite);
+  response.status(200).send({ msg: 'OK' });
 }
 
 async function updateVisite(req, res) {
-    let DateVisite = req.body.DateVisite;
-    let IdLogement = req.body.IdLogement;
-    let IdClient = req.body.IdClient;
+  let DateVisite = req.body.DateVisite;
+  let IdLogement = req.body.IdLogement;
+  let IdClient = req.body.IdClient;
 
-    db.updateGarage(
-        req.visite.id,
-        DateVisite,
-        IdLogement,
-        IdClient
-    );
+  await db.updateVisite(req.visite.id, DateVisite, IdLogement, IdClient);
 
-    res.status(200).json({ msg: "OK" });
+  res.status(200).send({ msg: 'OK' });
 }
 
 async function deleteVisite(req, res) {
-    await db.deleteVisite(req.visite.id);
-    res.json({ message: "Visite supprimé" });
+  await db.deleteVisite(req.visite.id);
+  res.json({ msg: 'Visite supprimée' });
 }
 
 module.exports = {
-    getVisite,
-    createVisite,
-    updateVisite,
-    deleteVisite
+  getVisite,
+  getAllVisitesForALogement,
+  createVisite,
+  updateVisite,
+  deleteVisite,
 };
--- server/src/models/visiteDAO.js
+++ server/src/models/visiteDAO.js
@@ -1,119 +1,127 @@
 const database = require('../helpers/dbconnect.js');
 
 async function getVisite(id) {
-    
-    return new Promise((resolve, reject) => {
-        let sql = "SELECT * FROM Visite WHERE IdVisite=?;";
-        database.getConnection((error, connection) => {
-            if (error)
-                reject(error);
-            connection.query(sql, [id], (error, results) => {
-                connection.release();
-                if (error)
-                    console.error(error.message);
-                if (results === undefined) {
-                    resolve(null);
-                } else if (results.length > 0) {
-                    resolve(results);
-                } else {
-                    resolve(null);
-                }
-            });
-        });
-
-    }).catch((error) => {
-        console.log(error);
-    });
-}
-
-
-async function createVisite(DateVisite, IdLogement, IdClient) {
-    
-    return new Promise((resolve, reject) => {
-        let sql = "INSERT INTO Logement (DateVisite, IdLogement, IdClient) values (?,?,?);";
-        database.getConnection((error, connection) => {
-            if(error){
-                console.error("Database connection error on createVisite");
-                reject(error);
-            }
-            connection.query(sql, [DateVisite, IdLogement, IdClient], (error) => {
-                connection.release();
-                if (error) {
-                    console.error(error.message);
-                    reject(error);
-                } else {
-                    resolve();
-                }
-            });
-        });
-
-    });
-}
-
-async function updateVisite(DateVisite, IdLogement, IdClient, IdVisite) {
-    
-    let parametres = new Array();
-    let requete = "UPDATE Visite SET ";
-    if(req.query.DateVisite !== null) {
-        requete += " DateVisite=?"
-        parametres.push(DateVisite)
+  return new Promise((resolve, reject) => {
+    let sql = 'SELECT * FROM Visite WHERE IdVisite=?;';
+    database.getConnection((error, connection) => {
+      if (error) {
+        console.error('Database connection error on getVisite', error.message);
+        reject(error);
       }
-      
-      if(req.query.IdLogement !== null) {
-        requete += " IdLogement=?"
-        parametres.push(IdLogement)
-      }
-
-      if(req.query.IdClient !==null) {
-          requete+="IdClient=?"
-          parametres.push(IdClient)
-      }
-      requete+=" WHERE IdVisite=?";
-      parametres.push(IdVisite)
-    database.getConnection((error, connection) =>{
-        if(error) console.error("Database connection error on updateVisite", error.message);
-    connection.query(requete,parametres, (error) => {
+      connection.query(sql, [id], (error, results) => {
         connection.release();
-        if (error) {
-            console.error(error.message);
-            return;
+        if (error) console.error(error.message);
+        if (results === undefined) {
+          resolve(null);
+        } else if (results.length > 0) {
+          resolve(results);
+        } else {
+          resolve(null);
         }
+      });
     });
-});
+  }).catch((error) => {
+    console.log(error);
+  });
+}
+
+async function createVisite(DateHeureVisite, IdLogement, IdClient) {
+  return new Promise((resolve, reject) => {
+    let sql =
+      'INSERT INTO Visite (DateHeureVisite, IdLogement, IdClient) values (?,?,?);';
+    database.getConnection((error, connection) => {
+      if (error) {
+        console.error(
+          'Database connection error on createVisite',
+          error.message
+        );
+        reject(error);
+      }
+      connection.query(
+        sql,
+        [DateHeureVisite, IdLogement, IdClient],
+        (error) => {
+          connection.release();
+          if (error) {
+            console.error(error.message);
+            reject(error);
+          } else {
+            resolve();
+          }
+        }
+      );
+    });
+  });
+}
+
+async function updateVisite(DateHeureVisite, IdLogement, IdClient, IdVisite) {
+  let parametres = new Array();
+  let requete = 'UPDATE Visite SET ';
+  if (req.query.DateVisite !== null) {
+    requete += 'DateVisite=? ';
+    parametres.push(DateHeureVisite);
+  }
+
+  if (req.query.IdLogement !== null) {
+    requete += 'IdLogement=? ';
+    parametres.push(IdLogement);
+  }
+
+  if (req.query.IdClient !== null) {
+    requete += 'IdClient=? ';
+    parametres.push(IdClient);
+  }
+  requete += 'WHERE IdVisite=?';
+  parametres.push(IdVisite);
+  database.getConnection((error, connection) => {
+    if (error)
+      console.error('Database connection error on updateVisite', error.message);
+    connection.query(requete, parametres, (error) => {
+      connection.release();
+      if (error) {
+        console.error(error.message);
+        return;
+      }
+    });
+  });
 }
 
 async function getAllVisitesForALogement(idLogement) {
-    let sql = "SELECT * FROM Visite WHERE IdLogement=?;";
-    database.getConnection((error, connection) =>{
-        if(error) console.error("Database connection error on deleteVisite", error.message);
+  let sql = 'SELECT * FROM Visite WHERE IdLogement=?;';
+  database.getConnection((error, connection) => {
+    if (error)
+      console.error(
+        'Database connection error on getAllVisitesForALogement',
+        error.message
+      );
     connection.query(sql, [idLogement], (error) => {
-        connection.release();
-        if (error) {
-            console.error(error.message);
-            return;
-        }
+      connection.release();
+      if (error) {
+        console.error(error.message);
+        return;
+      }
     });
-});
+  });
 }
 async function deleteVisite(id) {
-    
-    let sql = "DELETE FROM Visite WHERE IdVisite=?;";
-    database.getConnection((error, connection) =>{
-        if(error) console.error("Database connection error on deleteVisite", error.message);
+  let sql = 'DELETE FROM Visite WHERE IdVisite=?;';
+  database.getConnection((error, connection) => {
+    if (error)
+      console.error('Database connection error on deleteVisite', error.message);
     connection.query(sql, [id], (error) => {
-        connection.release();
-        if (error) {
-            console.error(error.message);
-            return;
-        }
+      connection.release();
+      if (error) {
+        console.error(error.message);
+        return;
+      }
     });
-});
+  });
 }
 
 module.exports = {
-    getVisite,
-    createVisite,
-    deleteVisite,
-    updateVisite,
-    getAllVisitesForALogement
-}
+  getVisite,
+  createVisite,
+  deleteVisite,
+  updateVisite,
+  getAllVisitesForALogement,
+};
