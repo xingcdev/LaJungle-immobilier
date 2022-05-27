7b5e05b66cef3da6aab5d285b63719ec5798d24e
Antoine Després <58844676+antoinedespres@users.noreply.github.com>
2022-05-27 23:01:45 +0200

    Ajout des routes garage au routeur

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,12 +3,14 @@ const { isAuthorized } = require('../middlewares/auth.js');
 
 const user = require('./user.js');
 const auth = require('./auth.js');
+const garage = require('./garage.js');
 const logement = require('./logement.js');
 const transaction = require('./transaction.js');
 const visite = require('./visite.js');
 
 router.use('/user', isAuthorized, user);
 router.use('/auth', auth);
+router.use('/garage', garage);
 router.use('/logement', logement);
 router.use('/transaction', transaction);
 router.use('/visite', visite);
