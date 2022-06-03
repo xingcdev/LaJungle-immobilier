46b1653ab3a985a97ca47e5159d9c38f20cfc498
xingcdev <60323726+xingcdev@users.noreply.github.com>
2022-06-03 19:50:20 +0200

    changer le theme en sombre

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,31 +7,38 @@ import LoginPage from '@/views/login/LoginPage/LoginPage';
 import NotFoundPage from '@/views/404/NotFoundPage/NotFoundPage';
 import { Dashboard } from '@components/layout';
 import HousingPage from './views/housings/HousingPage/HousingPage';
-import { RequireAuth } from '@context/auth';
+// import { RequireAuth } from '@context/auth';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
-function App() {
+const darkTheme = createTheme({
+	palette: {
+		mode: 'dark',
+	},
+});
+
+export default function App() {
 	return (
-		<div className="App">
-			<Routes>
-				<Route
-					path="/"
-					element={
-						<RequireAuth>
+		<ThemeProvider theme={darkTheme}>
+			<div className="App">
+				<Routes>
+					<Route
+						path="/"
+						element={
+							// <RequireAuth>
 							<Dashboard />
-						</RequireAuth>
-					}
-				>
-					<Route index element={<HomePage />}></Route>
-					<Route path="housings/:housingId" element={<HousingPage />}></Route>
-					<Route path="transactions" element={<TransactionsPage />}></Route>
-					<Route path="visits" element={<VisitsPage />}></Route>
-				</Route>
+							// </RequireAuth>
+						}
+					>
+						<Route index element={<HomePage />}></Route>
+						<Route path="housings/:housingId" element={<HousingPage />}></Route>
+						<Route path="transactions" element={<TransactionsPage />}></Route>
+						<Route path="visits" element={<VisitsPage />}></Route>
+					</Route>
 
-				<Route path="/login" element={<LoginPage />}></Route>
-				<Route path="*" element={<NotFoundPage />}></Route>
-			</Routes>
-		</div>
+					<Route path="/login" element={<LoginPage />}></Route>
+					<Route path="*" element={<NotFoundPage />}></Route>
+				</Routes>
+			</div>
+		</ThemeProvider>
 	);
 }
-
-export default App;
