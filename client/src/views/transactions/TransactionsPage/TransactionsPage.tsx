ad0606d6964f11c2a936d869d1b33a40265ef415
chocolayta <90750781+chocolayta@users.noreply.github.com>
2022-06-04 15:50:55 +0200

    Modify ListTransactions.tsx for mui Stack

--- client/src/views/transactions/TransactionsPage/TransactionsPage.tsx
+++ client/src/views/transactions/TransactionsPage/TransactionsPage.tsx
@@ -1,11 +1,11 @@
 import styles from './TransactionsPage.module.scss'
-import TransactionsList from '../TransactionsList/TransactionsList'
+//import TransactionsList from '../TransactionsList/TransactionsList'
 
 export default function TransactionsPage() {
 	return (
 		<section className={styles.page}>
 			<h1 className={styles.pageTitle}>Transactions effectuées</h1>
-			<TransactionsList />
+			{/* <TransactionsList /> */}
 		</section>
 	);
 }
