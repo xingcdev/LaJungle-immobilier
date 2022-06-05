9d928456c091c886d2ed1d8238228b2c421b6b52
xingcdev <60323726+xingcdev@users.noreply.github.com>
2022-06-05 10:28:17 +0200

    re design transactions page

--- client/src/views/transactions/TransactionsList/TransactionsList.tsx
+++ client/src/views/transactions/TransactionsList/TransactionsList.tsx
@@ -2,21 +2,18 @@ import styles from './TransactionsList.module.scss';
 import Stack from '@mui/material/Stack';
 import { Loading } from '@/components/feedback';
 import TransactionCard from '../TransactionsCard/TransactionsCard';
-/*import {
-	Table,
-	TableHead,
-	TableBody,
-	TableRow,
-	TableHeader,
-	TableData,
-} from '@/components/display';*/
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 interface TransactionsProps {
 	transactions: any | null;
 	isLoading: boolean;
-	setTransactionsData: (newTransactions: any) => void;
 }
-
 
 function Transactions(props: TransactionsProps) {
 	if (props.isLoading) return <Loading />;
@@ -25,81 +22,44 @@ function Transactions(props: TransactionsProps) {
 		<p className={styles.emptyMessage}>Transactions non trouvées :(</p>
 	);
 
-	function addTransactions(newTransactions: any) {
-		props.setTransactionsData((existingData: any) => {
-			// Overwrite only the values in existingData.
-			// 'newHousing' data has so much values that we don't need in 'existingData'
-			if (!existingData) return props.setTransactionsData([newTransactions]);
-			const keys = Object.keys(existingData[0]);
-			let computedTransaction: Record<string, any> = {};
-			keys.map((key) => {
-				computedTransaction[key] = newTransactions[key];
-			});
-
-			// todo: remove this if the response of logement/create returns 'nbGarages'
-			//computedTransaction['nbGarages'] = 0;
-
-			props.setTransactionsData([...existingData, computedTransaction]);
-		});
-	}
+	if (!props.transactions || !props.transactions.length) return emptyMessage;
 
 	return (
-		<Stack spacing={2}>
-			{!props.transactions || !props.transactions.length
-				? emptyMessage
-				: props.transactions?.map((transaction: any) => (
-					<Stack spacing={6}>
-						<TransactionCard
-							//key={transaction.id}
-							adresse={transaction.adresse}
-							idTransaction={transaction.idTransaction}
-							prixVente={transaction.prixVente}
-							pourcentageCommission={transaction.pourcentageCommission}
-							nomClient={transaction.nomClient}
-							montantCommission={transaction.montantCommission}
-						// logementId: number;
-						// prixVent: number;
-						// pourcentageCommission: number;
-						/>
-						{/* <TransactionCard */}
-					</Stack>
-				)
-				)
-			}
-		</Stack>
+		<TableContainer component={Paper}>
+			<Table sx={{ minWidth: 650 }} aria-label="simple table">
+				<TableHead>
+					<TableRow>
+						<TableCell align="center">idTransaction</TableCell>
+						<TableCell align="center">Prix de la vente</TableCell>
+						<TableCell align="center">Commission</TableCell>
+						<TableCell align="center">Logement</TableCell>
+						<TableCell align="center">Client</TableCell>
+						<TableCell align="center">Commission</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{props.transactions.map((transaction: any) => (
+						<TableRow
+							key={transaction.idTransaction}
+							sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+						>
+							<TableCell component="th" scope="row" align="center">
+								{transaction.idTransaction}
+							</TableCell>
+							<TableCell align="center">{transaction.prixVente}</TableCell>
+							<TableCell align="center">
+								{transaction.pourcentageCommission}
+							</TableCell>
+							<TableCell align="center">{transaction.adresse}</TableCell>
+							<TableCell align="center">{transaction.nomClient}</TableCell>
+							<TableCell align="center">
+								{transaction.montantCommission}
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</TableContainer>
 	);
-
-	/*return (
-		<TableRow>
-			<TableData>1232132</TableData>
-			<TableData>1232132</TableData>
-			<TableData>1232132</TableData>
-			<TableData>1232132</TableData>
-		</TableRow>
-	);*/
 }
 export default Transactions;
-
-/*export default function TransactionsList() {
-	const rows = [
-		<TransactionRow />,
-		<TransactionRow />,
-		<TransactionRow />,
-		<TransactionRow />,
-	];
-
-	return (
-		<Table>
-			<TableHead>
-				<TableRow>
-					<TableHeader>Id client</TableHeader>
-					<TableHeader>Logement</TableHeader>
-					<TableHeader>Prix vente (€)</TableHeader>
-					<TableHeader>Commission (%)</TableHeader>
-				</TableRow>
-			</TableHead>
-			<TableBody>{rows}</TableBody>
-		</Table>
-	);
-}*/
-
--- client/src/views/transactions/TransactionsPage/TransactionsPage.tsx
+++ client/src/views/transactions/TransactionsPage/TransactionsPage.tsx
@@ -1,46 +1,27 @@
-import styles from './TransactionsPage.module.scss'
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 import TransactionsList from '../TransactionsList/TransactionsList';
 import { useFetchGet } from '@hooks/fetching';
-//import TransactionsList from '../TransactionsList/TransactionsList'
 
 function TransactionsPage() {
-	const { data, setData, isLoading, error } = useFetchGet(
+	const { data, isLoading, error } = useFetchGet(
 		`${process.env.REACT_APP_API_URL}/transaction/getAll`
 	);
 
 	for (let i = 0; i < data.length; i++) {
-		data[i].montantCommission = 1000 + ((data[i].pourcentageCommission / 100) * data[i].prixVente);
+		data[i].montantCommission =
+			1000 + (data[i].pourcentageCommission / 100) * data[i].prixVente;
 	}
 	if (error) return <p>{error}</p>;
 
 	return (
-		<section className={styles.page}>
-			<h1 className={styles.pageTitle}>Transactions disponibles</h1>
-			<li className={styles.test}>
-				<span className={styles.test}><h5 >idTransaction</h5></span>
-				<span className={styles.test}><h5>Prix de la vente</h5></span>
-				<span className={styles.test}><h5>Commission</h5></span>
-				<span className={styles.test}><h5>Logement</h5></span>
-				<span className={styles.test}><h5>Client</h5></span>
-				<span className={styles.test}><h5>Commission</h5></span>
-			</li>
-			<TransactionsList
-				isLoading={isLoading}
-				transactions={data}
-				setTransactionsData={setData}
-			/>
-		</section>
+		<Box component="section">
+			<Typography component="h1" variant="h4" gutterBottom>
+				Transactions disponibles
+			</Typography>
+			<TransactionsList isLoading={isLoading} transactions={data} />
+		</Box>
 	);
 }
 
 export default TransactionsPage;
-
-
-// export default function TransactionsPage() {
-// 	return (
-// 		<section className={styles.page}>
-// 			<h1 className={styles.pageTitle}>Transactions effectuées</h1>
-// 			{/* <TransactionsList /> */}
-// 		</section>
-// 	);
-// }
